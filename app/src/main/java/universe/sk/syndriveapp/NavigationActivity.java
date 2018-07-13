dfe95185d8e35bfd39f996217ca5b5c8cee22b10
Megha K.C <meghakc10@gmail.com>
2018-07-13 12:22:27 +0530

    Adding maps

--- app/src/main/java/universe/sk/syndriveapp/NavigationActivity.java
+++ app/src/main/java/universe/sk/syndriveapp/NavigationActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.view.View;
@@ -122,6 +123,11 @@ public class NavigationActivity extends AppCompatActivity
             startActivity(new Intent(this,MainActivity.class));
 
 
+        }else if(id == R.id.nav_conatctus){
+
+            ContactUsFragment contactUsFragment = new ContactUsFragment();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.flMain, contactUsFragment).commit();
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
