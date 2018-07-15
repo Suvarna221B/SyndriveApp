8f6a677d7de3fb3c334f38f24431a9551432e135
SrividyaKK <SrividyaKK@users.noreply.github.com>
2018-07-15 10:44:37 +0530

    Removed email from edit profile

--- app/src/main/java/universe/sk/syndriveapp/EditProfileActivity.java
+++ app/src/main/java/universe/sk/syndriveapp/EditProfileActivity.java
@@ -2,12 +2,22 @@ package universe.sk.syndriveapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class EditProfileActivity extends AppCompatActivity {
+
+    EditText etName, etDOB, etBloodGroup, etPassword, etNewPassword, etConfirmNewPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        etName = findViewById(R.id.etName);
+        etDOB = findViewById(R.id.etDOB);
+        etBloodGroup = findViewById(R.id.etBloodGroup);
+        etPassword = findViewById(R.id.etPassword);
+        etNewPassword = findViewById(R.id.etNewPassword);
+        etConfirmNewPassword = findViewById(R.id.etConfirmNewPassword);
     }
 }
