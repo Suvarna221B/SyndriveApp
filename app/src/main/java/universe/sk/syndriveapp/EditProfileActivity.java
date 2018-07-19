04636ad00b2e6f266ee9422ac9ad80d17c4248fc
SrividyaKK <SrividyaKK@users.noreply.github.com>
2018-07-19 14:15:41 +0530

    Added camera skeleton for profile pic

--- app/src/main/java/universe/sk/syndriveapp/EditProfileActivity.java
+++ app/src/main/java/universe/sk/syndriveapp/EditProfileActivity.java
@@ -1,13 +1,16 @@
 package universe.sk.syndriveapp;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 public class EditProfileActivity extends AppCompatActivity {
 
     EditText etName, etDOB, etBloodGroup, etEmail, etPassword, etNewPassword, etConfirmNewPassword;
+    FloatingActionButton fabEdit, fabSave, fabProfilePic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +31,19 @@ public class EditProfileActivity extends AppCompatActivity {
         etPassword = findViewById(R.id.etPassword);
         etNewPassword = findViewById(R.id.etNewPassword);
         etConfirmNewPassword = findViewById(R.id.etConfirmNewPassword);
+        fabEdit = findViewById(R.id.fabEdit);
+        fabSave = findViewById(R.id.fabSave);
+        fabProfilePic = findViewById(R.id.fabProfilePic);
 
         etEmail.setEnabled(false);
         etEmail.setText("username@email.com");
+
+        fabProfilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 }
